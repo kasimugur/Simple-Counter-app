3b485e108f7923020ce6422377ea17a3d1a0dcbe
kasimugur <kasiugur7@gmail.com>
2024-08-31 23:11:35 +0300

    card css and container css added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react"
 import '../css/css/container.css'
+import TodoCard from "./TodoCard"
 export default function Todo() {
   const [value, setValue] = useState('')
   console.log(value)
   return (
     <>
       <div className="container">
-
         <div className="header">
           <h1>todo list app</h1>
           <input value={value} onChange={(e) => setValue(e.target.value)} type="text" className="input" />
         </div>
-    
+        <ul>
+          <TodoCard />
+        </ul>
       </div>
     </>
   )
--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,7 +1,19 @@
 export default function TodoCard() {
   return (
     <>
-    TodoCard
+      <div className="card">
+        <div className="card-header">
+          <h1>önemli</h1>
+        </div>
+        <div className="card-body">
+          
+            <span className="done">yapıldı</span>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <span className="edit">düzenle</span>
+            <span className="delete">sil</span>
+        
+        </div>
+      </div>
     </>
   )
 }
